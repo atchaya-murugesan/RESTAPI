a521a0d84b40aec43ea4cd0c00676d2e16b9eddd
Atchaya <150216177+atchaya-murugesan@users.noreply.github.com>
2024-03-01 22:47:49 +0530

    DAY 8

---
+++ DAY 8/EXCERCISE 3/excercise3/src/main/java/com/example/excercise3/Excercise3Application.java
@@ -0,0 +1,13 @@
+package com.example.excercise3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excercise3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Excercise3Application.class, args);
+	}
+
+}
